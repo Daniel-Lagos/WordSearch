5e5cfb9aebc1ffe54f7d69a7cf1f17dc5524c14f
Daniel-Lagos01 <d.f.l.m.0126@gmail.com>
2020-07-29 11:37:31 -0500

    Fill the word search

--- components/word-search-table/word-search.js
+++ components/word-search-table/word-search.js
@@ -1,17 +1,47 @@
 import styles from './word-search.module.css'
 
-const words=[]
+const words = [
+    ['a', 'b','a', 'b','a', 'b','a', 'b','a', 'b'],
+    ['a', 'b','a', 'b','a', 'b','a', 'b','a', 'b'],
+    ['a', 'b','a', 'b','a', 'b','a', 'b','a', 'b'],
+    ['a', 'b','a', 'b','a', 'b','a', 'b','a', 'b'],
+    ['a', 'b','a', 'b','a', 'b','a', 'b','a', 'b'],
+    ['a', 'b','a', 'b','a', 'b','a', 'b','a', 'b'],
+    ['a', 'b','a', 'b','a', 'b','a', 'b','a', 'b'],
+    ['a', 'b','a', 'b','a', 'b','a', 'b','a', 'b'],
+    ['a', 'b','a', 'b','a', 'b','a', 'b','a', 'b'],
+    ['a', 'b','a', 'b','a', 'b','a', 'b','a', 'b']
+]
 
-const generateWords=()=>{
-
+const generateWords = () => {
+    return (
+        <>
+            {
+                words.map((row, index) => {
+                    return (
+                        <tr key={index}>
+                            {
+                                row.map((word, index) => {
+                                    return (
+                                        <td key={index}>{word}</td>
+                                    )
+                                })
+                            }
+                        </tr>
+                    )
+                })
+            }
+        </>
+    )
 }
+
 const WordSearch = () => {
     return (<>
-            <table className={styles.tableWord}>
-                <tbody>
-                    generateWords
-                </tbody>
-            </table>
+        <table className={styles.tableWord}>
+            <tbody>
+                {generateWords()}
+            </tbody>
+        </table>
     </>);
 
 }
