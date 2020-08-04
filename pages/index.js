8a2d8f7d99e2aeee485736be337bc857a3f711fd
Daniel-Lagos01 <d.f.l.m.0126@gmail.com>
2020-08-03 22:33:01 -0500

    Implement word-search generator

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import GameButton from "../components/buttons/game-button";
 import Head from "next/head";
 import MainTitle from "../components/main/main-tittle";
 import Score from "../components/score/score";
+import wordsearch from "../utils/word-search-logic";
 
 const Index = () => {
     return (<>
@@ -35,3 +36,46 @@ const Index = () => {
 };
 
 export default Index;
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+export const getServerSideProps = async (context) => {
+    const wordsJSON = await import('../utils/words.json');
+    const words = wordsJSON.default || [];
+    const randomWords = shuffle(words);
+    const selectedWords = randomWords.slice(0, 8);
+    const wordsSortedByLength = selectedWords.sort((a, b) => {
+        return a.word.length > b.word.length ? -1 : 1;
+    });
+    const size = Math.ceil(wordsSortedByLength[0].word.length * 1.25);
+    const wordsStrings = wordsSortedByLength.map((it) => {
+        return it.word
+    })
+    const search = await wordsearch(wordsStrings, size, size);
+    if (!search) return {props: {}};
+    return {
+        props: {
+            puzzle: search.grid,
+            solved: search.solved,
+            placed: search.placed,
+            originalWords: wordsSortedByLength
+        }
+    };
+};
